757137310cf43d04ebc64321166566ce48c528e0
Zelin Zhou <z273zhou@uwaterloo.caa>
2020-10-21 18:52:54 -0400

    Fix Deployment Issue

--- src/util/IDExtractor.js
+++ src/util/IDExtractor.js
@@ -1,7 +1,8 @@
 export const extractor = (url) => {
+    // eslint-disable-next-line
     var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     var match = url.match(regExp);
-    if (match && match[2].length == 11) {
+    if (match && match[2].length === 11) {
         return match[2]
     } else {
         return false
